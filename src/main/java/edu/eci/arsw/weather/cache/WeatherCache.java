5e27a97fee9974c6fcffcbb993c704b09a9a0938
Alejandro Toro Daza <skullzo@outlook.com>
2021-04-08 09:14:22 -0500

    Código Fuente

--- src/main/java/edu/eci/arsw/weather/cache/WeatherCache.java
+++ src/main/java/edu/eci/arsw/weather/cache/WeatherCache.java
@@ -1,9 +1,14 @@
 package edu.eci.arsw.weather.cache;
 import edu.eci.arsw.weather.connection.HttpConnection;
+import edu.eci.arsw.weather.connection.WeatherException;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class WeatherCache {
     @Autowired
     HttpConnection http;
+    public String getCity(String name) throws IOException, WeatherException {
+        return temp;
+    }
 }
